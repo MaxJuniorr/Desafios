d85201217a8dee5e5bb24dd9d947634092641c43
Max Júnior <maxjr.dev@gmail.com>
2022-12-01 09:10:04 -0300

    Ajuste

--- Atividades/02.py
+++ Atividades/02.py
@@ -1,8 +1,6 @@
 import re
-texto = '(091)91234 5678 91 91234 5678 45565 91-91234-5678 cavalo (91) 91234-5678'
-expressão = r'\(?\d{2,3}\)?[\s\-]?\d{5}[\s\-]\d{4}'
-x = re.findall(expressão, texto)
-y = [re.sub(r'\(0','', i) for i in x]
-nova = [re.sub(r'\D', '', i) for i in y]
-formatados = [(f'({i[:2]}) {i[3:7]} {i[7:]}') for i in nova]
+texto = '(091)91204 5678 91 91234 5678 45565 91-91234-5678 cavalo (91) 91234-5678'
+x = re.findall(r'\(?\d{2,3}\)?[\s\-]?\d{5}[\s\-]\d{4}', texto)
+z = [re.sub(r'^\(?0|\D', '', i) for i in x]
+formatados = [(f'({i[:2]}) {i[3:7]} {i[7:]}') for i in z]
 print(formatados)
